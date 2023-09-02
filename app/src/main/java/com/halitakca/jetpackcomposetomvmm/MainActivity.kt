ed26f9b03265dd8a6fbd57b42ed10aaa213817c9
Halit Akca <halitakca070@gmail.com>
2023-09-02 18:38:20 +0300

    State Structure.

--- app/src/main/java/com/halitakca/jetpackcomposetomvmm/MainActivity.kt
+++ app/src/main/java/com/halitakca/jetpackcomposetomvmm/MainActivity.kt
@@ -3,11 +3,17 @@ package com.halitakca.jetpackcomposetomvmm
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.halitakca.jetpackcomposetomvmm.ui.theme.JetpackComposeToMVMMTheme
@@ -32,6 +38,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Sayfa(){
+    val sayac = remember { mutableStateOf(0) }
+
+    Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text = "Sayaç: ${sayac.value}")
+        
+        Button(onClick = { sayac.value += 5}) {
+            Text(text = "Sayacı arttır.")
+        }
+    }
 
 }
 
